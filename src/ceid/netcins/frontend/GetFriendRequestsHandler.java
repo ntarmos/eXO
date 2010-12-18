f73e763f4a578289e0b1a84f4c6731d963be3ac7
Nikos Ntarmos <ntarmos@cs.uoi.gr>
2010-12-18 03:11:02 +0200

    GetFriendRequests done

---
+++ src/ceid/netcins/frontend/GetFriendRequestsHandler.java
@@ -0,0 +1,41 @@
+package ceid.netcins.frontend;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.Vector;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.util.ajax.JSON;
+
+import rice.p2p.commonapi.Id;
+
+import ceid.netcins.CatalogService;
+import ceid.netcins.user.FriendRequest;
+
+public class GetFriendRequestsHandler extends CatalogFrontendAbstractHandler {
+
+	public GetFriendRequestsHandler(CatalogService catalogService,
+			Hashtable<String, Vector<String>> queue) {
+		super(catalogService, queue);
+	}
+
+	@Override
+	public void handle(String arg0, Request baseRequest, HttpServletRequest request,
+			HttpServletResponse response) throws IOException, ServletException {
+		Vector<FriendRequest> friendRequests = catalogService.getUser().getPendingIncomingFReq();
+		HashSet<Id> friendReqIDs = new HashSet<Id>();
+		if (friendRequests != null)
+			for (FriendRequest fr : friendRequests)
+				friendReqIDs.add(fr.getUID());
+
+		response.setContentType("application/json");
+		response.setStatus(HttpServletResponse.SC_OK);
+		baseRequest.setHandled(true);
+		response.getWriter().write(JSON.toString(friendReqIDs));
+	}
+}
