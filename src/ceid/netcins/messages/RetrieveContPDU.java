68bb1b663e8bf1d3922c90e8ffc99a3cf75f5d86
Nikos Ntarmos <ntarmos@cs.uoi.gr>
2010-12-23 06:41:13 +0200

    Clean-up RetrieveContPDU

--- src/ceid/netcins/messages/RetrieveContPDU.java
+++ src/ceid/netcins/messages/RetrieveContPDU.java
@@ -13,30 +13,18 @@ import rice.p2p.commonapi.Id;
  */
 public class RetrieveContPDU implements Serializable {
 
-	// The user Id
-	// private Id uid;
-
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = -3861889468881922351L;
 
-	// The checksum
+	// The content ID
 	private Id contentId;
 
 	// Flag for Tagclouds
-	// TODO : Implement it in the requests
 	private boolean cloudflag;
 
 	public RetrieveContPDU(Id contentId) {
-		// this.uid = uid;
 		this.contentId = contentId;
 		this.cloudflag = true;
 	}
-
-	// public Id getUID(){
-	// return uid;
-	// }
 
 	public Id getContentId() {
 		return contentId;
