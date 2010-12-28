2ce76d352910fc913fa776015f53dbaaedae2195
Nikos Ntarmos <ntarmos@cs.uoi.gr>
2010-12-29 00:41:35 +0200

    Teach bin/post.sh about "eXO::reqId" and "eXO::Processing"

--- bin/post.sh
+++ bin/post.sh
@@ -5,14 +5,29 @@ if [ $# -ne 1 -a $# -ne 2 ]; then
 	exit 1;
 fi
 
-if [ $# = 2 ]; then
-	/usr/bin/curl \
-		-i -H "Accept: application/json" -X POST \
-		-d "$2" \
-		"http://localhost:8080/servlet/$1/" 2>/dev/null | tail -2
-else
-	/usr/bin/curl \
-		-i -H "Accept: application/json" -X POST \
-		"http://localhost:8080/servlet/$1/" 2>/dev/null | tail -2
-fi
-echo; echo
+EXORESP=`
+	if [ $# = 2 ]; then
+		/usr/bin/curl \
+			-i -H "Accept: application/json" -X POST \
+			-d "$2" \
+			"http://localhost:8080/servlet/$1/" 2>/dev/null | tail -1
+	else
+		/usr/bin/curl \
+			-i -H "Accept: application/json" -X POST \
+			"http://localhost:8080/servlet/$1/" 2>/dev/null | tail -1
+	fi;`
+
+if [ ! -z `echo "$EXORESP" | grep "eXO::reqID"` ]; then
+	EXOREQID="$EXORESP";
+	echo "Got eXO::reqID. Long-polling..."
+	while true; do
+		EXORESP=`/usr/bin/curl \
+			-i -H "Accept: application/json" -X POST \
+			-d "eXO_data=${EXOREQID}" \
+			"http://localhost:8080/servlet/$1/" 2>/dev/null | tail -1`
+		if [ -z `echo "$EXORESP" | grep "eXO::Processing"` ]; then
+			break;
+		fi
+	done;
+fi;
+echo "$EXORESP"
